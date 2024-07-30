fce9df652bba04db312351065c5b8e3f08d5a42b
Andrew Mareschal <83591657+18101988@users.noreply.github.com>
2024-07-30 12:11:02 -0500

    Add props inventory tutorial bases (#4)
    * feat(props): add hotbar to bottom of screen
    * feat(props): add inventory to top-left
    * feat(props): add inventory toggle with Tab
    * feat(props): allow dropping item from inventory
    * feat(props): allow item pickup

---
+++ player/player.gd
@@ -0,0 +1,91 @@
+extends CharacterBody3D
+
+#this is use to signal to toggle_inventor to trigger
+signal toggle_inventory()
+
+@export var inventory_data: InventoryData
+@export var equip_inventory_data: InventoryDataEquip
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+ 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var health: int = 5
+
+var look_rot: Vector2
+
+@onready var head: Camera3D = $Camera3D
+@onready var interact_ray:RayCast3D = $Camera3D/InteractRay
+
+
+func _ready():
+	PlayerManager.player = self
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	
+	#as far as I can tell, this is a part of the mouselook section
+	if event is InputEventMouseMotion: #this is looking for a mouse moving event as well as if the free_mouse_cursor is false.
+		print("mouselook is on")
+		look_rot.y -= (event.relative.x * 0.2)
+		look_rot.x -= (event.relative.y * 0.2)
+		look_rot.x = clamp(look_rot.x, -80, 90)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+		
+	#if you press "tab" it will "emit" the toggle_inventory signal(sends out the signal)
+	if Input.is_action_just_pressed("inventory"):
+		print("message  sent")
+		toggle_inventory.emit()
+		#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		#free_mouse_cursor = 1
+	#elif Input.is_action_just_pressed("inventory") and free_mouse_cursor == 1:
+		#print("free mouse cursor is zero")
+		#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		#free_mouse_cursor = 0
+		
+		
+	if Input.is_action_just_pressed("interact"):
+		interact()
+ 
+func interact() -> void:
+	if interact_ray.is_colliding():
+		interact_ray.get_collider().player_interact()
+		print("we are interacting")
+
+func get_drop_position() -> Vector3:
+	var direction = -head.global_transform.basis.z
+	return head.global_position + direction
+
+func heal(heal_value: int) -> void:
+	health += heal_value
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+ 
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+ 
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+ 
+	move_and_slide()
+	
+	#this is also apart of the mouselook functionality
+	var plat_rot = get_platform_angular_velocity()
+	look_rot.y += rad_to_deg(plat_rot.y * delta)
+	head.rotation_degrees.x = look_rot.x
+	rotation_degrees.y = look_rot.y
---
+++ scenes/main.gd
@@ -0,0 +1,72 @@
+extends Node
+
+const PickUp = preload("res://item/pick_up/pick_up.tscn")
+
+var cursor_visible = 0
+
+@onready var player: CharacterBody3D = $Player
+@onready var inventory_interface: Control = $UI/InventoryInterface
+@onready var hot_bar_inventory: PanelContainer = $UI/HotBarInventory
+
+
+
+func _ready() -> void:
+	#the signal that was emited from player.gd will reach this toggle_inventory signal and trigger
+	#it to connect to main.gd and fire the function called toggle_inventory_interface
+	player.toggle_inventory.connect(toggle_inventory_interface)
+	inventory_interface.set_player_inventory_data(player.inventory_data)
+	inventory_interface.set_equip_inventory_data(player.equip_inventory_data)
+	inventory_interface.force_close.connect(toggle_inventory_interface)
+	hot_bar_inventory.set_inventory_data(player.inventory_data)
+	
+	for node in get_tree().get_nodes_in_group("external_inventory"):
+		node.toggle_inventory.connect(toggle_inventory_interface)
+
+#this is the function that is attached to the signal fired by toggle_inventory up in the 
+# _ready() funciton
+func toggle_inventory_interface(external_inventory_owner = null) -> void:
+	#inventory_interface.visible = not inventory_interface.visible
+	
+	
+	
+	#hot_bar_inventory.show()
+	inventory_interface.show()
+	
+	if cursor_visible == 0:
+		#pass
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		print("first if %s", cursor_visible)
+		cursor_visible = 1
+		
+		#hot_bar_inventory.show()
+	elif cursor_visible == 1:
+		#pass
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		print("second if %s", cursor_visible)
+		cursor_visible = 0
+		#hot_bar_inventory.hide()
+	
+	
+	#func toggle_inventory_interface() -> void:
+	#
+	#inventory_interface.visible = not inventory_interface.visible
+	#
+	#if inventory_interface.visible:
+	#	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	#	#hot_bar_inventory.hide()
+	#else:
+	#	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	#	hot_bar_inventory.show()
+		
+	if external_inventory_owner and inventory_interface.visible:
+		inventory_interface.set_external_inventory(external_inventory_owner)
+	else:
+		inventory_interface.clear_external_inventory()
+
+
+func _on_inventory_interface_drop_slot_data(slot_data) -> void:
+	var pick_up = PickUp.instantiate()
+	pick_up.slot_data = slot_data
+	pick_up.position = player.get_drop_position()
+	add_child(pick_up)
+	print(pick_up.slot_data)
