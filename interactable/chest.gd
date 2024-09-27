534511aeb8970d77117dd0414ebb2ea08eed36bf
Andrew Mareschal <mareschal_andrew@live.com>
2024-09-27 13:13:34 -0600

    update

--- interactable/chest.gd
+++ interactable/chest.gd
@@ -5,4 +5,6 @@ signal toggle_cursor_interaction(external_inventory_owner)
 @export var inventory_data: InventoryData
 
 func player_interact() -> void:
+	#print("I am getting goods")
 	toggle_cursor_interaction.emit(self)
+	
